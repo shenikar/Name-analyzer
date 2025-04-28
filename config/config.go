0130ee719a5c09ec3bbb5a22175a73eed53674e6
shenikar <shenikar@student.21-school.ru>
2025-04-28 17:20:12 +0300

    added config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	DBDSN     string
+	Port      string
+	LogLevel  string
+	RateLimit int
+}
+
+func NewConfig() (*Config, error) {
+	rateLimitStr := os.Getenv("RATE_LIMIT")
+	rateLimit, err := strconv.Atoi(rateLimitStr)
+	if err != nil {
+		return nil, err
+	}
+	return &Config{
+		DBDSN:     os.Getenv("DB_DSN"),
+		Port:      os.Getenv("PORT"),
+		LogLevel:  os.Getenv("LOG_LEVEL"),
+		RateLimit: rateLimit,
+	}, nil
+
+}
