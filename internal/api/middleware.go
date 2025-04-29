b1e55b997f4c2f9a50649a4a2fabd5fccc1eca5f
shenikar <shenikar@student.21-school.ru>
2025-04-29 21:48:22 +0300

    added middleware

---
+++ internal/api/middleware.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LoggingMiddleware(logger *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			logger.Printf("Request: %s %s %s", r.Method, r.URL.Path, time.Since(start))
+		})
+	}
+}
+
+func RecoverMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
